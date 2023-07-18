393d8e95287965f456627ac78abf615b474305a0
poljudy <franklinjin229@gmail.com>
2023-07-18 09:39:33 -0700

    clean code

--- server.js
+++ server.js
@@ -19,27 +19,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
-// app.get('/api.js', async (req, res) => {
-//     try {
-//         const response = await axios.get('https://apis.google.com/js/api.js?onload=__iframefcb739257');
-//         res.setHeader('Content-Type', 'application/javascript');
-//         res.send(response.data);
-//     } catch (error) {
-//         res.status(500).send('Error fetching script');
-//     }
-// });
-// Routes
-// 1 test route
-// app.get('/', (req, res) => {
-//     res.status(200).json({ message: "welcome to Blood Bank App" })
-// })
-
-// use for localhost
-// app.use('/api/v1/test', require('./routes/testRoutes'))
-// app.use('/api/v1/auth', require('./routes/authRoutes'))
-// app.use('/api/v1/inventory', require('./routes/inventoryRoutes'))
-// app.use('/api/v1/analytics', require('./routes/analyticsRoutes'))
-// app.use('/api/v1/admin', require('./routes/adminRoutes'))
 
 // used for render cloud
 app.use('/test', require('./routes/testRoutes'))
@@ -47,13 +26,6 @@ app.use('/auth', require('./routes/authRoutes'))
 app.use('/inventory', require('./routes/inventoryRoutes'))
 app.use('/analytics', require('./routes/analyticsRoutes'))
 app.use('/admin', require('./routes/adminRoutes'))
-
-// // STATIC FOLDER
-// app.use(express.static(path.join(__dirname, './client/build')));
-// /// STATIC ROUTES
-// app.get('*', function (req, res) {
-//     res.sendFile(path.join(__dirname, './client/build/index.html'));
-// })
 
 //port
 const PORT = process.env.PORT || 8080;
